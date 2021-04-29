661ce6fc6dc0f403405efb6478db00fa1bd6b455
Ryan Jeffrey <ryan@ryanmj.xyz>
2021-04-29 16:30:59 -0700

    Calculator.

---
+++ calc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def add(a, b):
+    return a + b
+
+def mult(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+def minus(a, b):
+    return a - b
+
+def modulo(a, b):
+    return a % b
+
